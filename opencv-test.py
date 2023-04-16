b0568f276c6e70e20c00907506bd08e35a1e84c4
Sai Sanjay Kottakota <97831658+sanjay7178@users.noreply.github.com>
2023-04-16 18:10:59 +0530

    Update opencv-test.py

--- opencv-test.py
+++ opencv-test.py
@@ -1,39 +1,69 @@
 import cv2
-import tensorflow as tf
+import numpy as np
+from tensorflow.keras.models import load_model
 
-# Load the model
-model = tf.keras.models.load_model('model.h5')
+# set the path to the model file
+model_path = 'vgg16_epochs_25.h5'
 
-# Open the camera feed
-cap = cv2.VideoCapture(0)
+# load the model
+model = load_model(model_path)
 
-# Loop over frames from the camera feed
+# set the input image size
+input_size = (224, 224)
+
+# set the input mean and standard deviation for normalization
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225]
+
+# set the input scale factor
+scale_factor = 1/255.0
+
+# set the labels for the classes
+class_labels = ['CELOSIA ARGENTEA L',  'CROWFOOT GRASS'	, 'PURPLE CHLORIS']
+
+# set the index of the camera to use
+camera_index = 0
+
+# create a VideoCapture object to capture frames from the camera
+cap = cv2.VideoCapture(camera_index)
+
+# loop over the frames from the camera
 while True:
-    # Read a frame from the camera
+    # read a frame from the camera
     ret, frame = cap.read()
-
-    # Preprocess the input image
-    img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    img = cv2.resize(img, (224, 224))
-    img = img / 255.0
-    img = tf.expand_dims(img, axis=0)
-
-    # Pass the preprocessed image through the model to obtain a prediction
-    pred = model.predict(img)
-
-    # Postprocess the prediction to obtain a final output
-    # In this example, we assume the model predicts a single class label
-    label = tf.argmax(pred, axis=1)
-    label = label.numpy()[0]
-
-    # Display the output on the camera feed
-    cv2.putText(frame, f'Class: {label}', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    
+    # resize the frame to the input size
+    frame = cv2.resize(frame, input_size)
+    
+    # convert the frame to a numpy array and normalize it
+    image = np.asarray(frame, dtype=np.float32)
+    image /= 255.0
+    image -= mean
+    image /= std
+    
+    # add a batch dimension to the input image
+    image = np.expand_dims(image, axis=0)
+    
+    # run the inference on the input image
+    prediction = model.predict(image)
+    
+    # get the predicted class label
+    predicted_class = np.argmax(prediction)
+    
+    # get the predicted class probability
+    predicted_prob = prediction[0][predicted_class]
+    
+    # draw the predicted class label and probability on the frame
+    label = '{}: {:.2f}%'.format(class_labels[predicted_class], predicted_prob*100)
+    cv2.putText(frame, label, (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+    
+    # show the frame
     cv2.imshow('Camera', frame)
-
-    # Exit the loop if the user presses the 'q' key
+    
+    # wait for a key press
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the resources
+# release the VideoCapture object and close all windows
 cap.release()
 cv2.destroyAllWindows()
